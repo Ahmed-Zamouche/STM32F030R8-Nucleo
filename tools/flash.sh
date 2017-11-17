5d1c26aff65f7edc11bbf30d59fdd87aa9aafca0
Ahmed Zamouche <ahmed.zamouche@assaabloy.com>
2017-11-17 16:02:32 +0100

    Update flash.sh

--- tools/flash.sh
+++ tools/flash.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-FILENAME=build/unittest/STM32F030R8-Nucleo.elf
+if [ $# -eq 0 ]; then
+   exit 1
+fi
 
-openocd -f board/st_nucleo_f0.cfg -c "program $FILENAME verify reset exit"
+FILENAME=$1
+    
+if [ -f $FILENAME ]; then
+   openocd -f board/stm32f4discovery.cfg -c "program $FILENAME verify reset exit"
+else
+	exit 2  
+fi
