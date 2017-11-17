baab5e0ffa7a392000cb19fa2ee0b94ab854751f
Ahmed Zamouche <ahmed.zamouche@assaabloy.com>
2017-11-17 16:02:02 +0100

    Update debug.sh

--- tools/debug.sh
+++ tools/debug.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-FILENAME=build/unittest/STM32F030R8-Nucleo.elf
+if [ $# -eq 0 ]; then  
+   exit 1
+fi
 
-if [ $1 = "socket" ]
-then
-    xterm -T "openocd -f board/st_nucleo_f0.cfg" -e "openocd -f board/st_nucleo_f0.cfg" &
-    OPENOCD_PID=$!
-    arm-none-eabi-gdb -tui -x tools/gdb-socket.cfg $FILENAME
-    kill $OPENOCD_PID
+FILENAME=$1
+
+if [ -f $FILENAME ]; then
+	if [ "$2" = "socket" ]; then
+	    xterm -T "openocd -f board/stm32f4discovery.cfg" -e "openocd -f board/stm32f4discovery.cfg" &
+	    OPENOCD_PID=$!
+	    arm-none-eabi-gdb -tui -x tools/gdb-socket.cfg $FILENAME
+	    kill $OPENOCD_PID
+	elif [ "$2" = "pipe" ]; then
+	    arm-none-eabi-gdb -tui -x tools/gdb-pipe.cfg $FILENAME
+	else
+		exit 3
+	fi
 else
-    arm-none-eabi-gdb -tui -x tools/gdb-pipe.cfg $FILENAME
+	exit 2	
 fi
