f85f349d87800b5d0e495743652ac6fbe8f30d16
Ahmed Zamouche <ahmed.zamouche@gmail.com>
2017-12-01 20:47:40 +0100

    high resolution timers

--- include/timer.h
+++ include/timer.h
@@ -7,7 +7,21 @@
 
 #ifndef INCLUDE_TIMER_H_
 #define INCLUDE_TIMER_H_
+#include <stdint.h>
+#include "timer.h"
 
-extern uint32_t usTick;
+enum Timer_e
+{
+	TIMER_1,
+
+	TIMER_NUM,
+
+};
+
+void timer_start(enum Timer_e);
+
+void timer_stop(enum Timer_e);
+
+uint32_t timer_counterValue(enum Timer_e);
 
 #endif /* INCLUDE_TIMER_H_ */
