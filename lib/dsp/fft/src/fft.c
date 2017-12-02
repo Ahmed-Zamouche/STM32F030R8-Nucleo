910333bd9df0f3adb6fd605e50ae2cce1ad2537f
Ahmed Zamouche <ahmed.zamouche@assaabloy.com>
2017-12-02 16:29:10 +0100

    Update fft.c
    parentheses around '-'

--- lib/dsp/fft/src/fft.c
+++ lib/dsp/fft/src/fft.c
@@ -45,7 +45,7 @@ static inline void Complex_Copy(Complex_t *des, Complex_t *src)
 __attribute__((always_inline))
 static inline void TwiddleFactor(Complex_t *c, size_t k, size_t log_2_n)
 {
-	size_t i =  (k << LOG_2_FFT_X_N-log_2_n);
+	size_t i =  k << (LOG_2_FFT_X_N-log_2_n);
 	c->re = twiddle_factor_re[i];
 	c->im = twiddle_factor_im[i];
 }
